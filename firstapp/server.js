41b16f922a303e4b53b98ae914ff43b079e3f451
aakashDeveloper <ahanda205@gmail.com>
2019-07-14 17:15:58 +0100

    A

---
+++ firstapp/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var os = require('os');
+
+var server = http.createServer(function(req,res){
+    res.write("<h1>Hi "+JSON.stringify(os.userInfo().username)+" Your are using mac "+ os.platform() +" of "+os.arch()+ " Architecture" );
+    res.end();
+})
+
+server.listen(2300);
